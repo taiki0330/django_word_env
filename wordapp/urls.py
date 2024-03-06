206ba7c2c8a3c090ecd7099dbca0a96569ff51d7
taiki0330 <jonas.taiki.0330@icloud.com>
2024-03-06 22:23:53 +0900

    try some template, but still not done

--- wordapp/urls.py
+++ wordapp/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import DivisionListView, CrimeListview, CrimeDetailView, generate_report
+from .views import DivisionListView, CrimeListview, CrimeDetailView, select_template
 
 urlpatterns = [
     path('', DivisionListView.as_view(), name='division_list'),
     path('crime_list/<int:division_id>', CrimeListview.as_view(), name='crime_list'),
     path('crime/<int:pk>', CrimeDetailView.as_view(), name='crime_detail'),
-    path('report/<int:pk>', generate_report, name='generate_report'),
+    path('report/<int:pk>', select_template, name='select_template'),
 ]
--- wordapp/views.py
+++ wordapp/views.py
@@ -3,6 +3,8 @@ from .models import Division, Crime
 from django.shortcuts import render, redirect
 from docxtpl import DocxTemplate
 from django.http import HttpResponse
+import os
+
 
 class DivisionListView(ListView):
     model = Division
@@ -22,14 +24,34 @@ class CrimeDetailView(DetailView):
 
 
 
-def generate_report(request, pk):
+# def generate_report(request, pk):
+#     crime = Crime.objects.get(pk=pk)
+#     doc = DocxTemplate("/Users/matsuzakidaiki/anaconda3/envs/django_word_env/wordproject/docx_templates/現場の写真撮影報告書.docx")
+#     context = {'title': crime.title, 'detail': crime.detail, 'division': crime.division.name}
+#     doc.render(context)
+
+#     response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
+#     response['Content-Disposition'] = 'attachment; filename="report.docx"'
+#     doc.save(response)
+
+#     return response
+
+def select_template(request, pk):
     crime = Crime.objects.get(pk=pk)
-    doc = DocxTemplate("/Users/matsuzakidaiki/anaconda3/envs/django_word_env/wordproject/docx_templates/犯罪事実.docx")
-    context = {'title': crime.title, 'detail': crime.detail, 'division': crime.division.name}
-    doc.render(context)
+    template_dir = '/Users/matsuzakidaiki/anaconda3/envs/django_word_env/wordproject/docx_templates'
+    templates = os.listdir(template_dir)
 
-    response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
-    response['Content-Disposition'] = 'attachment; filename="report.docx"'
-    doc.save(response)
+    if request.method == 'POST':
+        selected_template = request.POST.get('template')
+        doc = DocxTemplate(template_dir + selected_template)
+        context = {'title': crime.title, 'detail': crime.detail}
+        doc.render(context)
 
-    return response
+        response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
+        response['Content-Disposition'] = f'attachment; filename="{selected_template}"'
+        doc.save(response)
+
+        return response
+    else:
+        return render(request, 'crime_detail.html', {'templates': templates, 'crime': crime})
+    
