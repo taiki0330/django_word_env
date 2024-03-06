123fcb21249ebac5f07db2b710d0d97221fd2d1d
taiki0330 <jonas.taiki.0330@icloud.com>
2024-03-06 18:02:40 +0900

    only one template, but absolutely done

--- wordapp/views.py
+++ wordapp/views.py
@@ -19,3 +19,17 @@ class CrimeListview(ListView):
 class CrimeDetailView(DetailView):
     model = Crime
     template_name = 'crime_detail.html'
+
+
+
+def generate_report(request, pk):
+    crime = Crime.objects.get(pk=pk)
+    doc = DocxTemplate("/Users/matsuzakidaiki/anaconda3/envs/django_word_env/wordproject/docx_templates/犯罪事実.docx")
+    context = {'title': crime.title, 'detail': crime.detail, 'division': crime.division.name}
+    doc.render(context)
+
+    response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
+    response['Content-Disposition'] = 'attachment; filename="report.docx"'
+    doc.save(response)
+
+    return response
