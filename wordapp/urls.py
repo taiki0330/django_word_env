f47a96a7a7921cb0a2365fcea8b61067a3d91fe7
taiki0330 <jonas.taiki.0330@icloud.com>
2024-03-29 14:44:32 +0900

    final commit

--- wordapp/urls.py
+++ wordapp/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import DivisionListView, CrimeListview, CrimeDetailView, CrimeCreate, CrimeDelete, CrimeUpdate, generate_report,suggest_crime_name, generate_text
+from .views import DivisionListView, CrimeListview, CrimeDetailView, CrimeCreate, CrimeDelete, CrimeUpdate, generate_report,suggest_crime_name
 urlpatterns = [
     path('', DivisionListView.as_view(), name='division_list'),
     path('crime_list/<int:division_id>', CrimeListview.as_view(), name='crime_list'),
@@ -9,5 +9,4 @@ urlpatterns = [
     path('crime/update/<int:pk>/', CrimeUpdate.as_view(), name='crime_update'),
     path('report/<int:pk>', generate_report, name='generate_report'),
     path('api/suggest/crime/', suggest_crime_name, name='suggest_crime_name'),
-    path('generate-text/', generate_text, name='generate_text'),
 ]
